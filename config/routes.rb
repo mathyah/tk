c471a78e3ef109cfa217a042065fbfd6643bcda6
mathyah <miguel@utp.edu.co>
2017-05-03 20:29:48 -0500

    Con bootstrap y sesiones de chat para cada usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :chatrooms
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :chatrooms do
+    resource :chatroom_users
+  end
+
+
   root to: "chatrooms#index"
 end
